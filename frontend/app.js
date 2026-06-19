5210164ac71a71ce4150c9f79ce5e4d2940021dc
habtamukidus96-create <habtamukidus96@gmail.com>
2026-06-20 02:16:15 +0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,46 @@
+let clinicId = localStorage.getItem("clinicId");
 
+async function login(){
+  const email = document.getElementById("email").value;
+
+  const res = await fetch("/api/login", {
+    method:"POST",
+    headers:{ "Content-Type":"application/json" },
+    body: JSON.stringify({ email })
+  });
+
+  const data = await res.json();
+
+  localStorage.setItem("clinicId", data.clinicId);
+
+  window.location.href = "dashboard.html";
+}
+
+async function book(){
+  await fetch(`/api/book/${clinicId}`, {
+    method:"POST",
+    headers:{ "Content-Type":"application/json" },
+    body: JSON.stringify({
+      name:name.value,
+      phone:phone.value,
+      date:date.value
+    })
+  });
+
+  alert("Appointment booked ✔");
+  load();
+}
+
+async function load(){
+  const res = await fetch(`/api/appointments/${clinicId}`);
+  const data = await res.json();
+
+  document.getElementById("appointments").innerHTML =
+    data.map(a =>
+      `<div class="card">${a.name} - ${a.phone} - ${a.date}</div>`
+    ).join("");
+}
+
+if(window.location.pathname.includes("dashboard")){
+  load();
+}
