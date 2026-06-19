c92a9582ac753c3c89a8b3ee12ef3ca54d6d3a4c
habtamukidus96-create <habtamukidus96@gmail.com>
2026-06-20 02:12:38 +0300

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,1 +1,9 @@
+import admin from "firebase-admin";
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+export const db = admin.firestore();
